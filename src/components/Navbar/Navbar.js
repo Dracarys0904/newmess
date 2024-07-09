50b1a9f6161c77799bc28b41c28fe01c3d0ceab4
Dracarys0904 <165887235+Dracarys0904@users.noreply.github.com>
2024-07-09 09:00:16 +0530

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logoImage from './logo.png';  // Assuming the file name is "white logo.png
+import logoImage from './logo.png'; // Assuming the file name is "logo.png"
 import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo-title">
-        <img className='ab' src={logoImage} alt="Logo" />  {/* Add alt text for accessibility */}
-        <h1>Sinhgad Technical Education Society's<br /> Mess Feedback System</h1>
+        <img className='logo' src={logoImage} alt="Logo" />
+        <div className='sinhgad'>
+          <h1>Sinhgad Technical Education Society's<br />Mess Feedback System</h1>
+        </div>
       </div>
+      
       <div className="links">
         <Link to="/">Login</Link>
         <Link to="/student">Student</Link>
